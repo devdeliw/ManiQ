4eb976388e1eb3c83ca7e88b692279fb883bcf97
devdeliw <devaldeliwala@Devals-MacBook-Pro.local>
2024-10-28 01:30:31 -0700

    added measurement and barrier gates. fully working circuit animation tool for 2 operand circuits...working on n<=4 operand gates now

--- src/circuit.py
+++ src/circuit.py
@@ -12,7 +12,8 @@ class Gates(Scene):
     def construct(self):
 
         ctext = self.multi('CU1', 0, 1, -3, color = 'BLUE_D', idxs = [0, 1],
-                           params = [1, 0])  
+                           params = [1, 0])
+        ctext = self.barrier(0, 2, 1)
         self.add(ctext)
 
     # default single qubit gates
@@ -34,21 +35,56 @@ class Gates(Scene):
 
         return gate
 
-
-    def measurement(self, x, y): 
+    def measure(self, x, y1, y2): 
         # measurement qubit
-        dot = Dot(radius = 0.05).move_to([x, y-0.1, 0])
+        dot = Dot(radius=0.05,
+                  color=GRAY_D).move_to([x, y1-0.1, 0])
         semicirc = Arc(fill_opacity=0,
                        angle=PI, 
-                       stroke_width = 7).scale(0.35).move_to(np.add(
+                       stroke_width=5, 
+                       color=GRAY_D).scale(0.35).move_to(np.add(
                            dot.get_center(), np.array([0, 0.1, 0])))
         line = Line(dot.get_center(), 
-                    np.array([x + 0.3, y + 0.3, 0]),
-                    stroke_width = 5)
-        square = Square(side_length = 1).move_to([x, y, 0])
-        group = VGroup(dot, semicirc, line, square)
+                    np.array([x + 0.3, y1 + 0.3, 0]),
+                    stroke_width=5,
+                    color=GRAY_D)
+        square = Square(side_length = 1,
+                        color=YELLOW_A,
+                        fill_color=YELLOW_A,
+                        fill_opacity=1).move_to([x, y1, 0])
+        group = VGroup(square, dot, line, semicirc)
 
-        return group
+        line_measure_1 = Line(np.array([x-0.07, y1, 0]),
+                              np.array([x-0.07, y2+0.4, 0]), 
+                              stroke_width = 5, 
+                              color = YELLOW_A)
+        line_measure_2 = Line(np.array([x+0.07, y1, 0]), 
+                              np.array([x+0.07, y2+0.4, 0]), 
+                              stroke_width =5, 
+                              color = YELLOW_A)
+        measure_tip = Triangle(color = YELLOW_A, 
+                               fill_color = YELLOW_A, 
+                               fill_opacity
+                               = 1).scale(0.3).rotate(60*DEGREES).move_to([x,
+                                                                           y2+0.4, 0])
+        gate = VGroup(line_measure_1, line_measure_2, measure_tip, group)
+        return gate
+
+    def barrier(self, x, y1, y2): 
+        # aesthetic barrier 
+        rect = Rectangle(width=0.5, 
+                         height=abs(y2-y1)+1,
+                         fill_color=GRAY_B,
+                         fill_opacity=0.8,
+                         stroke_width=0).move_to(
+                                 [x,min(y1,y2)+abs(y1-y2)/2, 0])
+        min_y, max_y = min(y1,y2), max(y1,y2)
+        dotted_line = DashedLine(start=np.array([x, min_y-0.5, 0]), 
+                                 end=np.array([x, max_y+0.5, 0]),
+                                 color=GRAY_E,
+                                 stroke_width=1.5)
+        barrier = VGroup(rect, dotted_line)
+        return barrier
 
     # controlled pauli gates
     # each has a different design 
@@ -119,7 +155,6 @@ class Gates(Scene):
         line = Line(start = np.array([x1, y1, 0]), 
                     end = np.array([x1, y2, 0]), 
                     color = BLUE_C)
-
         if params: 
             y_param = min(y1,y2) + 1
             if params[1]: 
@@ -149,7 +184,6 @@ class Gates(Scene):
         line = Line(start = np.array([x1,y1,0]),
                     end = np.array([x1,y2,0]), 
                     color = BLUE_B)
-
         gate = VGroup(cross1, cross2, line)
         return gate
 
@@ -186,7 +220,6 @@ class Gates(Scene):
         # gate spanning multiple qubits
         label = MathTex(rf"{name}", 
                         font_size = 60).move_to([x1+0.5, min(y1,y2) + abs(y1-y2) / 2, 0])
-
         group = VGroup(label)
 
         if params: 
@@ -218,31 +251,63 @@ class Gates(Scene):
 class BuildCircuit(Scene):
     # build a visual manim circuit from a qiskit QuantumCircuit() object
     def construct(self, run_time=0.5):
-        qc = QuantumCircuit(5)
-        qc.h(0)
-        qc.x(1)
-        qc.y(2)
-        qc.z(3) 
-        qc.cx(4, 2)
-        qc.swap(1, 4)
-        qc.rxx(0, 0, 4)
-        mobjects = self.build(qc)
-        
-        circuit = VGroup(*mobjects)
-        wire_pos = list(range(-qc.num_qubits + 1, qc.num_qubits, 2)) 
 
-        wires = VGroup()
+        qc = random_circuit(5, max_operands = 2, depth = 3)
+        qc.measure_all()
+
+
+        mobjects = self.build(qc)
+        circuit = VGroup(*mobjects)
+
+        wire_pos = list(range(-qc.num_qubits+1, qc.num_qubits, 2)) 
+        cwires = VGroup()
+        if qc.num_clbits != 0: 
+            min_qwire = min(wire_pos)
+            c_wire_pos = min(wire_pos) - 2
+            cwires.add(Line(start=np.array(
+                                [circuit.get_center()[0]-circuit.width/2-0.3, c_wire_pos+0.03, 0]), 
+                            end=np.array(
+                                [circuit.get_center()[0]+circuit.width/2+0.3, c_wire_pos+0.03, 0]), 
+                            stroke_width=3, 
+                            color=YELLOW_A))
+            cwires.add(Line(start=np.array(
+                                [circuit.get_center()[0]-circuit.width/2-0.3,
+                                 c_wire_pos-0.06, 0]), 
+                            end=np.array(
+                                [circuit.get_center()[0]+circuit.width/2+0.3,
+                                 c_wire_pos-0.06, 0]),
+                            stroke_width=3, 
+                            color=YELLOW_A))
+            cwires.add(Line(start=np.array(
+                                [circuit.get_center()[0]-circuit.width/2-0.05,
+                                 c_wire_pos-0.1, 0]), 
+                            end=np.array(
+                                [circuit.get_center()[0]-circuit.width/2+0.05,
+                                 c_wire_pos+0.1, 0]), 
+                            stroke_width=5, 
+                            color=YELLOW_A))
+            cwires.add(MathTex(rf"C", 
+                               font_size=55).move_to(
+                                   [circuit.get_center()[0]-circuit.width/2-1, 
+                                    c_wire_pos, 0]))
+            cwires.add(MathTex(rf"{qc.num_clbits}", 
+                               font_size=35).move_to(
+                                   [circuit.get_center()[0]-circuit.width/2-0.15, 
+                                    c_wire_pos+0.25, 0]))
+
+
+        qwires = VGroup()
         idx = qc.num_qubits
         for wire in wire_pos: 
-            wires.add(Line(start=np.array([circuit.get_center()[0]-circuit.width/2 - 0.3, wire, 0]), 
-                             end=np.array([circuit.get_center()[0]+circuit.width/2 + 0.3, wire, 0]), 
-                             stroke_width = 2))
-            wires.add(MathTex(rf"q_{idx-1}", 
+            qwires.add(Line(start=np.array([circuit.get_center()[0]-circuit.width/2-0.3, wire, 0]), 
+                             end=np.array([circuit.get_center()[0]+circuit.width/2+0.3, wire, 0]), 
+                             stroke_width=5))
+            qwires.add(MathTex(rf"q_{idx-1}", 
                               font_size = 55).move_to(
-                                  [circuit.get_center()[0]-circuit.width/2 - 1, wire, 0]))
+                                  [circuit.get_center()[0]-circuit.width/2-1, wire, 0]))
             idx-=1
         
-        circuit_full = VGroup(wires, circuit).move_to([0, 0, 0])
+        circuit_full = VGroup(cwires, qwires, circuit).move_to([0, 0, 0])
         scaling_factor = min(config.frame_width/circuit_full.width,
                              config.frame_height/circuit_full.height)
         circuit_full.scale(scaling_factor)
@@ -267,7 +332,7 @@ class BuildCircuit(Scene):
                         else: 
                             return category, gate['latex'], 'MAROON_C'
 
-        categories, names, params, colors, idxs = [], [], [], [], []
+        categories, names, params, colors, qbits, cbits = [], [], [], [], [], []
         for instruction in qc.data:
             category, latex, color = categorize_gate(instruction.operation.name)
             categories.append(category)
@@ -276,14 +341,16 @@ class BuildCircuit(Scene):
             params.append([i for i in instruction.operation.params if
                            isinstance(i, (float, np.floating))])
 
-            idxs.append([qc.find_bit(qubit).index for qubit in instruction.qubits])
+            qbits.append([qc.find_bit(qubit).index for qubit in instruction.qubits])
+            cbits.append([qc.find_bit(clbit).index for clbit in instruction.clbits])
 
         circuit_data = pd.DataFrame({
             'category': categories,
             'names': names,
             'colors': colors,
             'params': params,
-            'idxs': idxs
+            'qbits': qbits,
+            'cbits': cbits
         })
 
         return circuit_data
@@ -298,7 +365,8 @@ class BuildCircuit(Scene):
         # algorithm to place quantum gates on a circuit
         for idx, elements in circuit_data.iterrows():
             category, name = elements['category'], elements['names']
-            params, qubits = elements['params'], elements['idxs']
+            params, qubits = elements['params'], elements['qbits']
+            cbits = elements['cbits']
             
             x, y, q0, q1, reduce = 0, 0, False, False, False
             if len(qubits) > 1:  
@@ -368,8 +436,10 @@ class BuildCircuit(Scene):
         circuit, x = [], np.zeros(num_qubits)
         for idx, elements in circuit_data.iterrows():
             category, name = elements['category'], elements['names']
-            params, qubits = elements['params'], elements['idxs']
+            params, qubits = elements['params'], elements['qbits']
+            cbits = elements['cbits']
             color = elements['colors']
+
             y_gate = wire_y_pos[qubits].tolist()
 
             if category == 'single_qubit_gates': 
@@ -413,9 +483,21 @@ class BuildCircuit(Scene):
                 if name.lower() == 'swap': 
                     init = Gates().swap(0, 0, 0)
                     if not np.allclose(x_coords, zeros): 
-                        x_gate += init.width/2
+                        x_gate += init.width/2   
                         x[min_idx:max_idx+1] += init.width/2
                     gate = Gates().swap(x_gate, y_gate[0], y_gate[1])
+                if name.lower() == 'measure': 
+                    init = Gates().measure(0, 0, 0)
+                    if not np.allclose(x_coords, zeros): 
+                        x_gate += init.width/2
+                        x[:] += init.width + gap 
+                    gate = Gates().measure(x_gate, y_gate[0], min(wire_y_pos)-2.08)
+                if name.lower() == 'barrier': 
+                    init = Gates().barrier(0, 0, 1)
+                    if not np.allclose(x_coords, zeros): 
+                        x_gate += init.width/2
+                        x[min_idx:max_idx+1] += init.width + gap
+                    gate = Gates().barrier(x_gate, min(y_gate), max(y_gate))
 
             elif category == 'cphase_gates': 
                 init = Gates().ctext(0, 0, 0, params=[name, params])
@@ -469,7 +551,7 @@ class BuildCircuit(Scene):
 
 
 if __name__ == "__main__":
-    qc = random_circuit(10, depth = 5, max_operands = 2)
+    qc = random_circuit(5, depth = 1, max_operands = 2)
     qc.rzz(0.4, 0, 3)
     qc.rxx(0.4, 4, 0)
     print(qc)
