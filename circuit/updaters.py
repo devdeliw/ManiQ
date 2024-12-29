8c6714a8e4adec8939b559911824b16b9dc5108d
devdeliw <devaldeliwala@Devals-MacBook-Pro.local>
2024-12-28 23:34:11 -0700

    general updates

--- circuit/updaters.py
+++ circuit/updaters.py
@@ -8,41 +8,44 @@ Helper Functions for configuring user interactions
 
 """ 
 
-def global_cursor_to_manim(scene): 
+def global_cursor_to_manim(scene, scaling_factor):
     """ 
-    Converts pixel coordinates in desktop-space to manim-space coordinates. 
-    
-    *Assumes the top-left of desktop screen is (0,0).* 
-    This is true for most graphical applications.
+    Converts pixel coordinates of cursor to manim-space coordinates. 
+
+    Assumes top --left-- of desktop screen is (0,0). Works on 
+    most modern OS.
 
     """
 
-    # Pixel positions 
+    # Global cursor positions
     x_global, y_global = pyautogui.position() 
-    # Top left of pyglet window
-    x_win, y_win = scene.renderer.window_position 
 
-    # Pixel positions within pyglet window 
-    x_local = x_global - x_win 
-    y_local = y_global - y_win  
+    # Top-left pyglet window position
+    x_win, y_win = scene.renderer.window.position 
 
-    # Renderer dimensions 
+    # Local coords within pyglet window
+    x_local = x_global - x_win
+    y_local = y_global - y_win
+
+    # Width of pyglet window
     w_width = scene.renderer.window.width 
-    w_height = scene.renderer.window.height 
+    w_height = scene.renderer.window.height
 
-    # Manim coordinate system dimensions 
-    mw = config.frame_width 
-    mh = config.frame_height 
+    # Manim system dimensions
+    mw = config.frame_width * scaling_factor
+    mh = config.frame_height * scaling_factor
 
-    # Normalizing 
-    x_norm = x_local / w_width 
-    y_norm = 1 - (y_local / w_height) # 1- because manim increases y as you move up
+    # Normalizing to within pyglet window
+    x_norm = x_local / w_width
+    # 1 - is because pyglet window increases y from bottom-up
+    y_norm = 1 - (y_local / w_height)
 
-    # Final Manim-space coordinates 
-    x_manim = x_norm * mw - mw / 2 
+    # Final manim-space cursor coordinates
+    x_manim = x_norm * mw - mw / 2
     y_manim = y_norm * mh - mh / 2
-
     return x_manim, y_manim
+
+
 
 def get_bounding_box(mobj): 
     """
